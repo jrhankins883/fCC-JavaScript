17afe4cbf4574c29e2010305e384b8dd68a0cccf
John Hankins <jrhankins883@yahoo.com>
2025-09-16 18:46:02 -0400

    Just a little beginner mini project

---
+++ trivia-bot/script.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "KubrickJCroft";
+const botLocation = "The Grid";
+const favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " isn't very easy.";
+
+console.log(codingFact);
+
+codingFact = "Very easy, " + favoriteLanguage + " isn't.";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " would like to move on now!";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
