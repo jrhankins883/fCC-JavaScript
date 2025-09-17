ada5db6b0f29c7a2595a77faf694538f3df23c66
John Hankins <jrhankins883@yahoo.com>
2025-09-17 17:45:24 -0400

    Completed a build a sentence mini JS project

---
+++ build-a-sentence/script.js
@@ -0,0 +1,21 @@
+let adjective = "ancient";
+let noun = "shark";
+let verb = "eating";
+let place = "deep ocean";
+let adjective2 = "radiant";
+let noun2 = "swimmers";
+
+const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "bubbly";
+noun = "clown";
+verb = "consuming bodies whole";
+place = "tower of dead cheerleaders";
+adjective2 = "giant";
+noun2 = "cheerleaders";
+
+const secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
