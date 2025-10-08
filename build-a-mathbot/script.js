d0b414fbbcaf9872af74817c2f268a503a6f52f4
John Hankins <jrhankins883@yahoo.com>
2025-10-08 17:36:46 -0400

    Used Math.ceil() to round a number assigned to a variable up and then log it to the console

--- build-a-mathbot/script.js
+++ build-a-mathbot/script.js
@@ -1,23 +1,21 @@
 const botName = "MathBot";
 const greeting = `Hi there! My name is ${botName} and I am here to teach you about the Math object!`;
-
 console.log(greeting);
 
 console.log("The Math.random() method returns a pseudo random number between 0 and less than 1.");
-
 const randomNum = Math.random();
 console.log(randomNum);
 
 console.log("Now, generate a random number between two values.");
 const min = 1;
 const max = 100;
-
 const randomNum2 = Math.random() * (max - min) + min;
 console.log(randomNum2);
 
 console.log("The Math.floor() method rounds the value down to the nearest whole integer.");
-
 const numRoundedDown = Math.floor(6.7);
 console.log(numRoundedDown);
 
 console.log("The Math.ceil() method rounds the value up to the nearest whole integer.");
+const numRoundedUp = Math.ceil(3.2);
+console.log(numRoundedUp)
