4ae02acbf5e3325b09b13c04c609b8f495822399
John Hankins <jrhankins883@yahoo.com>
2025-10-08 17:18:09 -0400

    Step 2: added another log to the console

--- build-a-mathbot/script.js
+++ build-a-mathbot/script.js
@@ -2,3 +2,5 @@ const botName = "MathBot";
 const greeting = `Hi there! My name is ${botName} and I am here to teach you about the Math object!`;
 
 console.log(greeting);
+
+console.log("The Math.random() method returns a pseudo random number between 0 and less than 1.");
