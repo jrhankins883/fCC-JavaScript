74023df4d2257d7158b6ab24868786b316ca8947
John Hankins <jrhankins883@yahoo.com>
2025-10-08 17:51:02 -0400

    Created two more variables that had a row of numbers and used Math.max() and .min() to get the number. Then logged both to the console

--- build-a-mathbot/script.js
+++ build-a-mathbot/script.js
@@ -27,3 +27,7 @@ const numRounded2 = Math.round(11.2);
 console.log(numRounded2);
 
 console.log("The Math.max() and Math.min() methods are used to get the maximum and minimum number from a range.");
+const maxNum = Math.max(3, 125, 55, 24);
+console.log(maxNum);
+const minNum = Math.min(6, 90, 14, 90, 2);
+console.log(minNum);
