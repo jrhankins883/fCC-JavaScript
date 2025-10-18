a1cc4ebc1fa8121d7b5b8f12c6f8af568b434250
John Hankins <jrhankins883@yahoo.com>
2025-10-18 19:28:17 -0400

    Started a JS lab called Build A Fortune Teller

---
+++ fortune-teller/script.js
@@ -0,0 +1,22 @@
+const fortune1 = "You will get to see another planet one day!";
+const fortune2 = "You will get to swim in the middle of the ocean one day (for fun).";
+const fortune3 = "You will see a portal to another dimension in your lifetime.";
+const fortune4 = "You will not go through the portal because you will make the smart decision.";
+const fortune5 = "You will marry the love of your life and experience these things together.";
+
+const randomNumber = 4;
+const selectedFortune;
+
+if (randomNumber === 1) {
+  fortune1 = selectedFortune;
+} else if (randomNumber === 2) {
+  fortune2 = selectedFortune;
+} else if (randomNumber === 3) {
+  fortune3 = selectedFortune;
+} else if (randomNumber === 4) {
+  fortune4 = selectedFortune;
+} else if (randomNumber === 5) {
+  fortune5 = selectedFortune;
+}
+
+console.log(selectedFortune)
