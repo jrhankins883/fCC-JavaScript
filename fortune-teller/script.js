f1af406b465ce0fd37865a1486f93c562fb836c3
John Hankins <jrhankins883@yahoo.com>
2025-10-31 17:18:29 -0400

    erledigt

--- fortune-teller/script.js
+++ fortune-teller/script.js
@@ -4,7 +4,7 @@ const fortune3 = "You will see a portal to another dimension in your lifetime.";
 const fortune4 = "You will not go through the portal because you will make the smart decision.";
 const fortune5 = "You will marry the love of your life and experience these things together.";
 
-const randomNumber = 4;
+let randomNumber = Math.floor(Math.random() * 5) + 1;
 let selectedFortune;
 
 if (randomNumber === 1) {
