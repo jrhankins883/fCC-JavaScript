8d9b17581a04a3caf0eb1262fc704fafaf14ffb6
John Hankins <jrhankins883@yahoo.com>
2025-10-31 11:30:22 -0400

    Cleaned up and corrected some code for the fortune teller assignment. Not finished yet.

--- fortune-teller/script.js
+++ fortune-teller/script.js
@@ -1,22 +1,22 @@
-const fortune1 = "You will get to see another planet one day!";
+let fortune1 = "You will get to see another planet one day!";
 const fortune2 = "You will get to swim in the middle of the ocean one day (for fun).";
 const fortune3 = "You will see a portal to another dimension in your lifetime.";
 const fortune4 = "You will not go through the portal because you will make the smart decision.";
 const fortune5 = "You will marry the love of your life and experience these things together.";
 
 const randomNumber = 4;
-const selectedFortune;
+let selectedFortune;
 
 if (randomNumber === 1) {
-  fortune1 = selectedFortune;
+  selectedFortune = fortune1;
 } else if (randomNumber === 2) {
-  fortune2 = selectedFortune;
+  selectedFortune = fortune2;
 } else if (randomNumber === 3) {
-  fortune3 = selectedFortune;
+  selectedFortune = fortune3;
 } else if (randomNumber === 4) {
-  fortune4 = selectedFortune;
+  selectedFortune = fortune4;
 } else if (randomNumber === 5) {
-  fortune5 = selectedFortune;
+  selectedFortune = fortune5;
 }
 
 console.log(selectedFortune)
