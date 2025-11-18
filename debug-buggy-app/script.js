3fc279528c1ea59a7acb3e3da29bc63fe581916c
John Hankins <jrhankins883@yahoo.com>
2025-11-18 15:27:53 -0500

    Completed

--- debug-buggy-app/script.js
+++ debug-buggy-app/script.js
@@ -1,17 +1,17 @@
-const firstResult = 5 + "10";
+const firstResult = 5 + 10;
 console.log(`5 + 10 = ${firstResult}`);
 
-const secondResult = "Eight" - 5;
+const secondResult = 8 - 5;
 console.log(`8 - 5 = ${secondResult}`);
 
-const thirdResult = true + 5;
+const thirdResult = 1 + 5;
 console.log(`1 + 5 = ${thirdResult}`);
 
-const fourthResult = false + 8;
+const fourthResult = 0 + 8;
 console.log(`0 + 8 = ${fourthResult}`);
 
-const fifthResult = "10" * 2;
+const fifthResult = 10 * 2;
 console.log(`10 * 2 = ${fifthResult}`);
 
-const sixthResult = null + 22;
+const sixthResult = 0 + 22;
 console.log(`0 + 22 = ${sixthResult}`);
