b618e070ae7827a60811c1603a58b2026c406318
John Hankins <jrhankins883@yahoo.com>
2025-11-19 17:34:50 -0500

    Currently stuck. Will come back and dig in further

--- email-masker/script.js
+++ email-masker/script.js
@@ -1,5 +1,12 @@
 let email = "kubrickjcroft@gmail.com"
 
 function maskEmail(email) {
-  
+  const [user, domain] = email.split("@");
+  const masked = user[0] + "*".repeat(user.length - 1);
+  return `${masked}@${domain}`;
 }
+
+maskEmail(email);
+console.log(email);
+maskEmail("apple.pie@example.com")
+console.log(email)
