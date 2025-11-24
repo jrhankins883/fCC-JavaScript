4e36ddf3f30a7140f48ca84462c54a5ab691b0f4
John Hankins <jrhankins883@yahoo.com>
2025-11-24 17:10:46 -0500

    Finished

--- email-masker/script.js
+++ email-masker/script.js
@@ -2,11 +2,12 @@ let email = "kubrickjcroft@gmail.com"
 
 function maskEmail(email) {
   const [user, domain] = email.split("@");
-  const masked = user[0] + "*".repeat(user.length - 1);
+  const masked = user[0] + "*".repeat(user.length - 2) + user.slice(-1);
   return `${masked}@${domain}`;
 }
 
-maskEmail(email);
-console.log(email);
-maskEmail("apple.pie@example.com")
-console.log(email)
+console.log(maskEmail(email));
+console.log(maskEmail("apple.pie@example.com"))
+console.log(maskEmail("freecodecamp@example.com"))
+console.log(maskEmail("info@test.dev"))
+console.log(maskEmail("user@domain.org"))
