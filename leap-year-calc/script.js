b5980396253f17a13cd3366289b590c5704bbc94
John Hankins <jrhankins883@yahoo.com>
2025-11-29 16:47:54 -0500

    Started a new lab building a leap year calculator

---
+++ leap-year-calc/script.js
@@ -0,0 +1,12 @@
+let year = 1900;
+
+function isLeapYear(year) {
+  if (year % 4 === 0 && year % 400 === 0) {
+    return `${year} is a leap year.`;
+  } else if (year % 100 === 0) {
+    return `${year} is not a leap year.`;
+  } 
+}
+
+const result = isLeapYear(year);
+console.log(result)
