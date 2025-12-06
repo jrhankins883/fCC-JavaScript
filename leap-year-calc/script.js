f46df2b16af3c739e1a3bf50d921ea5856c7f528
John Hankins <jrhankins883@yahoo.com>
2025-12-06 16:24:24 -0500

    Finished

--- leap-year-calc/script.js
+++ leap-year-calc/script.js
@@ -1,11 +1,13 @@
 let year = 1900;
 
 function isLeapYear(year) {
-  if (year % 4 === 0 && year % 400 === 0) {
+  if (year % 400 === 0) {
     return `${year} is a leap year.`;
   } else if (year % 100 === 0) {
     return `${year} is not a leap year.`;
-  } 
+  } else if (year % 4 === 0) {
+    return `${year} is a leap year.`;
+  }
 }
 
 const result = isLeapYear(year);
